6c4e75728650e143b5e391a785b6cfdbacf6367f
UzunPaula <paula.uzun2003@gmail.com>
2023-08-25 09:48:35 +0300

    Add car management logic

---
+++ DealershipManagerApi/DealershipManagerApi/Repositories/ICarRepository.cs
@@ -0,0 +1,13 @@
+﻿using DealershipManagerApi.Models;
+
+namespace DealershipManagerApi.Repositories
+{
+    public interface ICarRepository
+    {
+        void Add(Car car);
+        Car? Get(Guid id);
+        List<Car> GetAll();
+        void Update(Guid carId, Car car);
+        void Delete(Guid id);
+    }
+}
---
+++ DealershipManagerApi/DealershipManagerApi/Services/CarService.cs
@@ -0,0 +1,40 @@
+﻿using DealershipManagerApi.Models;
+using DealershipManagerApi.Repositories;
+using System.Net;
+
+namespace DealershipManagerApi.Services
+{
+    public class CarService : ICarService
+    {
+        private readonly ICarRepository _carRepository;
+        public CarService(ICarRepository carRepository)
+        {
+            _carRepository = carRepository;
+        }
+          
+        public void Add(Car car)
+        {
+            _carRepository.Add(car);
+        }
+
+        public void Delete(Guid id)
+        {
+            _carRepository.Delete(id);
+        }
+
+        public Car? Get(Guid id)
+        {
+            return _carRepository.Get(id);
+        }
+
+        public List<Car> GetAll()
+        {
+            return _carRepository.GetAll();
+        }
+
+        public void Update(Guid carId, Car car)
+        {
+            _carRepository.Update(carId, car);
+        }
+    }
+}
